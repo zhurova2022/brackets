ca10ec41b43ce96a89ee2a27c4f665e3203cac4e
Alisa Zhurova <zhurova2021@gmail.com>
2023-01-27 16:42:10 +0300

    modified index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {};
+  const result = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for(let j = 0; j < str.length; j++){
+    result[result.length - 1] === brackets[str[j]] && result.length > 0 ? result.pop() : result.push(str[j]);
+  }
+  return result.length === 0;
 }
